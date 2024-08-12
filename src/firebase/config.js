4909b1c05db2469be8a22df01f3b18d471f606a1
silverstar323 <silverstars323@gmail.com>
2024-08-12 15:52:44 -0700

    Get data from Firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDK1WcmlTLCHHsQFeqrXnbBqtkbaH0hBV4",
   authDomain: "writenode-b9d88.firebaseapp.com",
@@ -13,5 +8,4 @@ const firebaseConfig = {
   appId: "1:362464121148:web:2c3f8bf7c33d5ef729ce39"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
